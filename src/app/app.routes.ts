c49a0b70cf8e00ab5562215188d022bf8d2e46a4
DabaSD <dabasambdiagne30@gmail.com>
2024-06-06 16:43:02 +0000

    Dashbord & Connexion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'accueil', component:  AccueilComponent,},
+    { path: 'connexion', component:  ConnexionComponent,},
+    { path: '', redirectTo: '/accueil', pathMatch: 'full' }
+];
