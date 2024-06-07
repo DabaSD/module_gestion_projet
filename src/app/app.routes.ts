f00468c56428d20ffc5fc91bb85b411bbd989b7d
DabaSD <dabasambdiagne30@gmail.com>
2024-06-07 01:14:14 +0000

    sprint 2 terminé

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { ConnexionComponent } from './connexion/connexion.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 export const routes: Routes = [
     { path: 'accueil', component:  AccueilComponent,},
     { path: 'connexion', component:  ConnexionComponent,},
+    { path: 'sidenav', component:  SideNavComponent,},
     { path: '', redirectTo: '/accueil', pathMatch: 'full' }
 ];
