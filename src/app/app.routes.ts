624608c01f060d4bd1a0495c1c3297c8bfa46d0f
DabaSD <dabasambdiagne30@gmail.com>
2024-06-14 08:16:18 +0000

    connexion + ajout de projet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
+import { AjoutProjetComponent } from './ajout-projet/ajout-projet.component';
 
 export const routes: Routes = [
     { path: 'accueil', component:  AccueilComponent,},
     { path: 'connexion', component:  ConnexionComponent,},
     { path: 'sidenav', component:  SideNavComponent,},
+    { path: 'ajoutProjet', component:  AjoutProjetComponent,},
     { path: '', redirectTo: '/accueil', pathMatch: 'full' }
 ];
