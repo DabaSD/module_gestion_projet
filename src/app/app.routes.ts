51efe397c28e8f300028de650879e532b1f896a7
DabaSD <dabasambdiagne30@gmail.com>
2024-06-27 22:32:21 +0000

    ressources humaines

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,15 @@ import { ListeRoleComponent } from './role/liste-role/liste-role.component';
 import { ProfilRoleComponent } from './role/profil-role/profil-role.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { AjoutProjetComponent } from './ajout-projet/ajout-projet.component';
+import { RessourceHumaineComponent } from './ressource-humaine/ressource-humaine.component';
 
 export const routes: Routes = [
     { path: 'accueil', component:  AccueilComponent,},
     { path: 'connexion', component:  ConnexionComponent,},
-    { path: 'accueil/liste-role', component:  ListeRoleComponent,},
-    { path: 'accueil/profil-role', component:  ProfilRoleComponent,},
+    { path: 'liste-role', component:  ListeRoleComponent,},
+    { path: 'profil-role', component:  ProfilRoleComponent,},
     { path: 'sidenav', component:  SideNavComponent,},
     { path: 'ajoutProjet', component:  AjoutProjetComponent,},
+    { path: 'ressourceHumaine', component:  RessourceHumaineComponent,},
     { path: '', redirectTo: '/accueil', pathMatch: 'full' }
 ];
---
+++ src/app/ressource-humaine/ressource-humaine.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SideNavComponent } from '../side-nav/side-nav.component';
+
+@Component({
+  selector: 'app-ressource-humaine',
+  standalone: true,
+  imports: [SideNavComponent],
+  templateUrl: './ressource-humaine.component.html',
+  styleUrl: './ressource-humaine.component.css'
+})
+export class RessourceHumaineComponent {
+
+}
